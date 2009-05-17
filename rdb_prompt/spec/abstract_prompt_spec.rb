abb615da40c67b29ddc733e2319741331dcf2915
Stephan Wehner <stephanwehner@gmail.com>
2009-05-17 06:30:59 -0700

    Adding another example to abstract_prompt_spec.rb

--- rdb_prompt/spec/abstract_prompt_spec.rb
+++ rdb_prompt/spec/abstract_prompt_spec.rb
@@ -1,7 +1,16 @@
 require  File.dirname(__FILE__) + '/spec_helper'
 
 describe RailsGoodies::RdbPrompt::AbstractPrompt do
-  it 'should raise error when database is empty' do
+  it 'should have an initializer that saves the two args' do
+    test_config = {'database' => 'tdb', :a => 1, :b => 2}
+    test_options = {:opt_1 => 1, :opt_2 => 2}
+    abstract_prompt = RailsGoodies::RdbPrompt::AbstractPrompt.new(test_config, test_options)
+    abstract_prompt.config.should == test_config
+    abstract_prompt.options.should == test_options
+  end
+
+  # Some checking on the value of database entry in the first hash is performed for an AbstractPrompt:
+  it 'should raise error when database is missing' do
     lambda {
       abstract_prompt = RailsGoodies::RdbPrompt::AbstractPrompt.new({}, {})
     }.should raise_error(RuntimeError, 'No database name found')
@@ -22,5 +31,6 @@ describe RailsGoodies::RdbPrompt::AbstractPrompt do
       abstract_prompt = RailsGoodies::RdbPrompt::AbstractPrompt.new({'database' => " \n"}, {})
     }.should raise_error(RuntimeError, 'Database name has whitespace')
   end
+
 end
 
